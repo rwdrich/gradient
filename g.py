c7735df55e204bf521a6abd88a40e423466b486d
Richard Davies <richard.davies@citrix.com>
2020-12-03 19:32:01 +0000

    Remove mypy

--- g.py
+++ g.py
@@ -8,21 +8,21 @@ def isIdle(self, pid, n):
 
     self.outputFile = "data.txt"
     # Calculating Memory
-    data: List[List[int]] = []
+    data = []
 #    for line in system.os(f"grep {pid} {self.outputFile} | tail -n {n}").splitlines():
  #       data.append(list(map(int, line.split())))
 
     sumMem = 0
     for row in data:
         sumMem += row[2]
-    averageMem: float = sumMem / n
-    averageTime: float = ( 1 + n ) / 2
+    averageMem = sumMem / n
+    averageTime = ( 1 + n ) / 2
 
     # trendline = (sum of x - averageX) (sum of y - averageY) / (sum of x - averageX) ^2
-    sumY: float = 0
+    sumY = 0
     for row in data:
         sumY += row[2] - averageMem
-    sumX: float = 0
+    sumX = 0
     for i in range(n + 1):
         sumX += i - averageTime
     trendline = ( sumX * sumY ) / ( sumX * sumX ) * averageMem
@@ -33,14 +33,14 @@ def g(pid, n):
 
     outputFile = "data.txt"
     # Calculating Memory
-    data: List[List[int]] = []
+    data = []
     cmd = f"grep ^{pid} {outputFile} | tail -n {n}"
     values = os.popen(cmd).read()
     for line in values.splitlines():
         data.append(list(map(int, line.split())))
 
     sumy = 0
-    sumxiyi: float = 0
+    sumxiyi = 0.0
     sumxi2 = 0
     sumx = 0
     for x in range(len(data)):
